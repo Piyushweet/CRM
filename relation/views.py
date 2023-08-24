da05648882ed877dc9db0160739ef38f993b35a6
Piyushweet <90750157+Piyushweet@users.noreply.github.com>
2023-08-24 09:52:49 +0530

    form

--- relation/views.py
+++ relation/views.py
@@ -1,6 +1,17 @@
-from django.shortcuts import render,HttpResponse
-
+from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 # Create your views here.
 
+
 def home(request):
-    return render(request,'index.html',{ })
+    
+    return render(request, 'index.html', {})
+
+
+def login_user(request):
+    pass
+
+
+def logout_user(request):
+    pass
